d16155246df9e40d9a1cb492331fe2fdb11de858
mohamedslama12 <mohamedslamanazih@gmail.com>
2025-04-16 10:24:13 +0100

    add image

---
+++ app/data/mounument.ts
@@ -0,0 +1,24 @@
+export const monuments = [
+    {
+      id: 'tour-eiffel',
+      nom: 'Tour Eiffel',
+      pays: 'France',
+      description: 'La Tour Eiffel est une tour de fer puddlé située à Paris.',
+      image: '/images/tour-eiffel.jpg',
+    },
+    {
+      id: 'taj-mahal',
+      nom: 'Taj Mahal',
+      pays: 'Inde',
+      description: 'Le Taj Mahal est un mausolée en marbre blanc situé à Agra.',
+      image: '/images/taj-mahal.jpg',
+    },
+    {
+      id: 'statue-liberte',
+      nom: 'Statue de la Liberté',
+      pays: 'États-Unis',
+      description: 'Symbole de liberté, elle est située à New York.',
+      image: '/images/statue-liberte.jpg',
+    },
+  ];
+  
