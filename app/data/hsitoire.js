2e2aff51cf3aaa62d83eed63819d36daee304dc9
mohamedslama12 <mohamedslamanazih@gmail.com>
2025-04-23 13:12:57 +0100

    add

---
+++ app/data/hsitoire.js
@@ -0,0 +1,34 @@
+/ data/histoire.js
+
+const histoires = [
+    {
+      id: 'eiffel',
+      nom: 'Tour Eiffel',
+      pays: 'France',
+      anneeConstruction: 1889,
+      description: `La Tour Eiffel est une tour de fer puddlé de 330 mètres de hauteur située à Paris. 
+      Construite par Gustave Eiffel pour l'Exposition universelle de 1889, elle est devenue un symbole emblématique de la France.`,
+      image: '/images/tour-eiffel.jpg'
+    },
+    {
+      id: 'colisee',
+      nom: 'Colisée',
+      pays: 'Italie',
+      anneeConstruction: 80,
+      description: `Le Colisée est un amphithéâtre romain situé au centre de Rome. Construit sous l'empereur Vespasien, 
+      il pouvait accueillir jusqu'à 50 000 spectateurs pour des combats de gladiateurs et autres spectacles.`,
+      image: '/images/colisee.jpg'
+    },
+    {
+      id: 'statue-liberte',
+      nom: 'Statue de la Liberté',
+      pays: 'États-Unis',
+      anneeConstruction: 1886,
+      description: `Offerte par la France aux États-Unis, la Statue de la Liberté est située sur Liberty Island à New York. 
+      Elle est un symbole d'accueil et de liberté pour les immigrants arrivant aux États-Unis.`,
+      image: '/images/statue-liberte.jpg'
+    }
+  ];
+  
+  export default histoires;
+  
