63c14ff66fd913b01eab06f2b75d39c39a299dc4
mohamedslama12 <mohamedslamanazih@gmail.com>
2025-04-23 13:30:11 +0100

    add

---
+++ app/pages/mounement/histoire.js
@@ -0,0 +1,10 @@
+// pages/histoire.js
+export default function Histoire() {
+    return (
+      <div>
+        <h1>Notre Histoire</h1>
+        <p>Tout a commencé en 2021, dans un petit garage...</p>
+      </div>
+    );
+  }
+  
