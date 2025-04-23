e1264288ef93aa5d137e6b1e437a17905f266441
mohamedslama12 <mohamedslamanazih@gmail.com>
2025-04-23 13:12:23 +0100

    modification

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <section>
-      <h2>Bienvenue à la Tour Eiffel</h2>
+     <center> <h2>Bienvenue à la Tour Eiffel</h2></center>
      <center> <Image
         src="/images/tour-eiffel.jpg" // assure-toi d’avoir cette image dans /public/images
         alt="La Tour Eiffel"
@@ -22,7 +22,7 @@ export default function HomePage() {
       <p>
         Découvrez son histoire, sa construction, et explorez une galerie unique à travers notre site.
       </p>
-      <h2>Bienvenue  a la taj  mahal</h2>
+      <center><h2>Bienvenue  a la taj  mahal</h2></center>
         <center> <Image
          src="/images/taj-mahal.jpg" // assure-toi d’avoir cette image dans /public/images
         alt="taj- mahal"
@@ -32,7 +32,7 @@ export default function HomePage() {
       /></center>
       <p>Sa construction commence en 1632, mais la date exacte de la fin des travaux demeure incertaine. Selon le chroniqueur officiel de Shâh Jahân, Abdul Hamid Lahori (en), le Taj Mahal est achevé à la fin de 1643 ou au début de 1644[10]. Mais une inscription dans l'entrée principale indique que la construction s'est achevée en 1648. L'État de l'Uttar Pradesh, qui a célébré officiellement le 350e anniversaire de l'édifice en 2004, affirme quant à lui que les travaux se sont achevés en 1654. Le chercheur Andrew Petersen, lui, mentionne prudemment que « les travaux ont duré plus de vingt ans »[11].</p>
       <p>En 2019, le monument a attiré plus de 6 millions et demi de touristes, dont plus de 800 000 étrangers</p>
-      <h2>statue liberte</h2>
+      <center><h2>statue liberte</h2></center>
      <center> <Image
          src="/images/statue-liberte.jpg"
 
@@ -46,9 +46,7 @@ export default function HomePage() {
       <p>Construite et assemblée en France, sur une idée en 1865 du juriste Édouard Lefebvre de Laboulaye, au moment d'une collecte du quotidien Le Phare de la Loire pour honorer la veuve d'Abraham Lincoln[4], la statue fut offerte par le peuple français aux Américains, en signe d'amitié, et dévoilée au grand jour le 28 octobre 1886 en présence du président des États-Unis, Grover Cleveland, pour le centenaire de la Déclaration d'indépendance américaine.</p>
       <nav>
         <ul>
-          <li><Link href="/histoire"> Histoire</Link></li>
-          <li><Link href="/galerie"> Galerie</Link></li>
-          <li><Link href="/infos"> Infos pratiques</Link></li>
+         
         </ul>
       </nav>
     </section>
